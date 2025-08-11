66d029b295bf9f82fc196342244abf2ee14747da
EduardoCastro <ecpcastro08@gmail.com>
2025-08-11 17:56:39 -0500

    Implementado funciones de agregar y mostrar la lista de amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let nombreAmigo = document.getElementById('amigo');
+let listaAmigos = document.getElementById('listaAmigos');
+
+function agregarAmigo(){
+    
+    if(nombreAmigo.value == ''){
+        alert("Por favor ingrese un nombre")
+    } else {
+        amigos.push(nombreAmigo.value);
+    }
+    limpiarEntrada();
+    verAmigos();
+}
+
+function limpiarEntrada(){
+    nombreAmigo.value = "";
+}
+
+function verAmigos(){
+    listaAmigos.innerHTML= "";
+
+    for (const amigo of amigos) {
+        let elementos = document.createElement('li');
+        elementos.textContent = amigo;
+        listaAmigos.appendChild(elementos);
+    }
+}
+
